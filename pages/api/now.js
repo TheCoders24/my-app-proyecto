556fd7159c4b7d217ade8950b37d6c522d958814
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-11-18 19:45:29 -0600

    Remove unnecessary blank line in /pages/api/now.js

--- pages/api/now.js
+++ pages/api/now.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Método no permitido' });
     }
-
     try {
         // Ejecuta la consulta para obtener la hora del servidor
         const result = await query('SELECT NOW()');
