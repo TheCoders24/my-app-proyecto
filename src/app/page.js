6d2bd5d2cf7f7abb7f3bfb5d08b70a41795d502e
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-11-20 13:27:05 -0600

    "Remove login.js file and update page.js to import and render LoginPage component"

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@ import Image from "next/image";
 import Landing from "./index";
 import products from "./products";
 import { appRouterContext } from "next/dist/server/route-modules/app-route/shared-modules";
+import LoginPage from "./loginpage";
+
 
 export default function Home() {
   return (
    //<Landing />
-    <products/>
+   //<products/>
+    <LoginPage/>
   );
 }
