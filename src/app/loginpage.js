db7c2187432a41b3ee6783e635084a05f08802c7
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-11-20 14:50:56 -0600

    "Added a space after 'Username:' label in LoginPage.js"

--- src/app/loginpage.js
+++ src/app/loginpage.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
           htmlFor="username"
           className="block text-gray-700 font-medium mb-2"
         >
-          Username:
+          Username: 
         </label>
         <input
           type="text"
