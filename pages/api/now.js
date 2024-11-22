ec1b8bc0bc6f403e3c703112c1113cb97ab164d5
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-11-21 19:13:28 -0600

    "Added whitespace and changed syntax in try block in now.js"

--- pages/api/now.js
+++ pages/api/now.js
@@ -5,11 +5,11 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Método no permitido' });
     }
-    try {
+    try 
+    {
         // Ejecuta la consulta para obtener la hora del servidor
         const result = await query('SELECT NOW()');
         console.log('Resultado de la consulta:', result.rows[0]);
-        
         // Devuelve la hora del servidor en el formato esperado
         res.status(200).json({ serverTime: result.rows[0].now }); // Asegúrate de acceder a la propiedad correcta
     } 
