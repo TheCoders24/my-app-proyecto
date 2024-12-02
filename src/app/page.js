3713a2d48e87f35daad75b965634c5672320f949
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-12-01 19:03:29 -0600

    "Deleted LoginPage, RegisterPage, and Principal components from pages directory and removed their imports from app/page.js. Replaced with new imports from components directory."

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Landing from "./components/index";
 import ProductosPage from "./components/products";
 import { appRouterContext } from "next/dist/server/route-modules/app-route/shared-modules";
-import LoginPage from "../../pages/loginpage";
-import RegisterPage from "../../pages/registerpages";
-import Principal from "../../pages/principal";
+import LoginPage from "./components/loginpage";
+import RegisterPage from "./components/registerpages";
+import Principal from "./components/principal";
 import DashboardPage from './../../pages/Dashboard'
+//import { Router } from "next/router";
 export default function Home() {
   return (
     <Principal/>
