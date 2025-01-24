94a976bbae6659f78885d1e2326f30cd21d76a5e
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-01-24 15:16:27 -0600

    add message

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
-        <h1 className="text-3xl font-bold mb-4">Bienvenido a Mi Proyecto</h1>
+        <h1 className="text-3xl font-bold mb-4">Bienvenido a mi sitio web</h1>
         <p className="text-gray-700 mb-6">
           Por favor, inicia sesión o regístrate para continuar.
         </p>
