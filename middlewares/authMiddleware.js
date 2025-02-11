d529040d6792b122e5e0ff9755efeb17b73c0c96
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-02-11 15:12:13 -0600

    function verifytoken

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -30,3 +30,20 @@ export default function authMiddleware(handler){
         }
     };
 }
+
+
+export default function VerifyToken(req, res, next){
+    const token = req.cookies.token; // Obtenemoos el Token de las Cookis
+
+    if(!token){
+        return res.status(401).json({ message: "No Autorizado"});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // Agregar el usuario decodificado al objeto "req"
+        next(); // Continuamos con la siguiente funciones
+    }catch(error){
+        res.status(401).json({message: "Token Invalido o Expirado"});
+    }
+}
