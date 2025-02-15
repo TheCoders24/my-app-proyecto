37416bbb8ce081428399c23688239a63aa651480
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-02-15 16:29:36 -0600

    correcion importacion conexiones

--- pages/api/report/report.js
+++ pages/api/report/report.js
@@ -1,5 +1,6 @@
 import toast from "react-hot-toast";
 import { query } from "../../../lib/db";
+import { data } from "autoprefixer";
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
@@ -9,9 +10,16 @@ export default async function handler(req, res) {
         // Consulta SQL para obtener el reporte
         const reportQuery = `SELECT producto_id, tipo, SUM(cantidad) AS total FROM Movimientos GROUP BY producto_id, tipo`;
         const report = await query(reportQuery);
-        res.status(200).json(report);
+        res.status(200).json({
+            message: "Reporte Generado Correctamente",
+            data: report  // <-- esto es lo que el cliente necesita recibir el cliente
+        });
+
     } catch (error) {
         console.error('Error generando reporte:', error);
-        res.status(500).json({ message: 'Error generando reporte' });
+        res.status(500).json({
+            message: 'Error generando reporte',
+            error: error.message // Opcional enviar detalles del error
+        });
     }
 }
