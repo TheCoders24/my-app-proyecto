d62d85323980cc6d5e76187c341c39c53b7a42cb
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-03-06 18:46:23 -0600

    add

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,23 +6,17 @@ export default function authMiddleware(handler){
     return async(req, res) => {
         //Extraemos el token del encabezado " Authorization"
         const token = req.headers.authorization?.split(" ")[1];
-        
         console.log(token);
-
         if(!token){
             // Devolemos un mensaje con un estado de codigo 401 y con un mensaje de no autorizado
             return res.status(401).json({ message: "No autorizado: Token no Proporcionado"});
         }
-        
-        try{
+        try
+        {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
             console.log(decoded);
-
             // Agregamos el usuario decodificado al objeto "Req" para usaro en la API
             req.user = decoded;
-
-
             // Continuamos con la logica de la api
             return handler(req,res)
         }catch(error){
@@ -31,15 +25,13 @@ export default function authMiddleware(handler){
     };
 }
 
-
 export default function VerifyToken(req, res, next){
     const token = req.cookies.token; // Obtenemoos el Token de las Cookis
-
     if(!token){
         return res.status(401).json({ message: "No Autorizado"});
     }
-
-    try{
+    try
+    {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded; // Agregar el usuario decodificado al objeto "req"
         next(); // Continuamos con la siguiente funciones
