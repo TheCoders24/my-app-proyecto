5a2150bea189814115ac11ff8d878b0c9df52e7b
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-03-09 19:55:34 -0600

    correcion de register

--- src/app/components/LoginForms.js
+++ src/app/components/LoginForms.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast"; // Importar para notificaciones
@@ -87,7 +88,6 @@ export default function LoginForm() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      {/* Contenedor de notificaciones Toast */}
       <Toaster
         position="top-center"
         toastOptions={{
@@ -102,14 +102,12 @@ export default function LoginForm() {
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
 
-        {/* Mensaje de éxito */}
         {success && (
           <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
             {success}
           </div>
         )}
 
-        {/* Mensaje de error */}
         {error && (
           <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
             {error}
@@ -141,10 +139,19 @@ export default function LoginForm() {
 
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mb-4"
           >
             Iniciar Sesión
           </button>
+
+          <Link href="/Register" passHref>
+            <button
+              type="button"
+              className="w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+            >
+              Registrar
+            </button>
+          </Link>
         </form>
       </div>
     </div>
